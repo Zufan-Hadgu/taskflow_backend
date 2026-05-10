b17bcbe71c6fa3f092585c845893ca561ad254a4
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-05-10 12:47:55 +0300

    add separate token service

---
+++ src/auth/token.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class TokenService {
+    constructor(private readonly jwtService: JwtService) {}
+    generateAccessToken(payload: any) {
+        return this.jwtService.sign({ payload });
+    }
+
+    verifyToken(token: string) {
+        return this.jwtService.verify(token);
+    }
+}
