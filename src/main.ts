66c50d749af4fe8a3ba358662f27832be711d9e2
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-05-10 12:45:53 +0300

    update  with validationpipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
     .setDescription('API for managing projects')
     .setVersion('1.0')
     .build();
+
+    app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,// this one remove extra field that is not in dto
+      forbidNonWhitelisted: true,//rejects invalid datas
+      transform: true, // transform payloads to DTO instances
+    }),
+  );
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); 
