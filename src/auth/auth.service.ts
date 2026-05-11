4c8376c5ae2c325f5b6183dfdcb99f85732131dc
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-05-11 14:51:56 +0300

    refactored the  validation function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { TokenService } from './token.service';
 import { LoginUserDto } from 'src/auth/dto/login-user.dto';
+import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -10,15 +11,17 @@ export class AuthService {
 
     async validateUser(email: string, password: string): Promise<any> {
 
-            if (email === 'zufanhadgu1@gmail.com' && password === '12341234') {
-                return { id: 1, email: email };
-            }   else{
-                return "Invalid credentials";
-            }
+            if (email !== 'zufanhadgu1@gmail.com' && password !== '12341234') {
+                throw new UnauthorizedException('Invalid credentials');
+               
+            }  
+            return { id: '1', email,};
     }
        
 
-    async login(user: any) {
+    async login(loginUserDto: LoginUserDto) {
+        const { email, password } = loginUserDto;
+        const user = await this.validateUser(email, password);
         const payload = 
         { 
             sub: user.id,
@@ -29,12 +32,18 @@ export class AuthService {
         };   
     }
 
-    async register(dto: any) {
-        // In a real application, you would save the user to a database
+    async register(registerUserDto: RegisterUserDto) {
+
+        const { name, email, password, confirmPassword } = registerUserDto;
+
+        if (password !== confirmPassword) {
+            throw new BadRequestException('Passwords do not match');
+        }
         const newUser = {
             id: Date.now().toString(),
-            email: dto.email,
-            password: dto.password, // In a real application, never store plain passwords
+            name: registerUserDto.name,
+            email: registerUserDto.email,
+            password: registerUserDto.password, 
         };
         this.users.push(newUser);
         return newUser;
