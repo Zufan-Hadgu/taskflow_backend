9a6e9425695c32052f7bfb45a111b9185a86c6b3
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-05-14 16:09:29 +0300

    Wired global configModule and other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { DatabaseProviderModule } from './providers/database/provider.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ProjectsModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig], 
+      isGlobal: true,     }),
+    DatabaseProviderModule, 
+    ProjectsModule, 
+    UsersModule, 
+    AuthModule, TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
