4b9ec206bc3d70b51ec8737f573934438551976f
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-05-15 09:56:07 +0300

    Added User entity

---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Project } from '../../projects/entities/project.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Project, (project) => project.user)
+  projects: Project[];
+}
