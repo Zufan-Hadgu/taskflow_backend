0c39e55ed57bc4c3bab4960029e35923d285a044
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-06-13 18:14:48 +0300

    feat(mail): register email queue in MailModule

---
+++ src/email/email.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { BullModule } from '@nestjs/bullmq';
+import { EmailProcessor } from './email.processor';
+
+@Module({
+  imports:[
+    BullModule.registerQueue({
+      name: 'email',
+    }),
+
+  ],
+  providers: [EmailService,EmailProcessor],
+  exports: [EmailService,BullModule,EmailModule],
+})
+export class EmailModule {}
